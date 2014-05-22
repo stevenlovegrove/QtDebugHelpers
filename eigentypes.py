d161d3bd85009ea5a99c58b2a58490a5f51c441b
Steven Lovegrove <stevenlovegrove@gmail.com>
2014-05-22 14:27:40 +0100

    Initial commit of Eigen and Sophus debug helpers.

---
+++ eigentypes.py
@@ -0,0 +1,41 @@
+############################################################################
+## Eigen (http://eigen.tuxfamily.org/) QtCreator Python Debug Helpers
+## Author: Steven Lovegrove
+############################################################################
+## These helpers have been tested with QT Creator 3.1.0 on OSX 10.9.2
+## Using lldb-310.2.37. My platform doesn't seem to recognise class namespaces,
+## and to function correctly these have been omitted.
+
+# Eigen::Matrix, expected function name qdump__Eigen__Matrix
+# This dumper will work for all template instantiations of Eigen::Matrix and
+# typedefs there-of such as Vector3f, MatrixXd, etc.
+def qdump__Matrix(d, value):
+    m_storage = value[0]["m_storage"]
+    m_data = m_storage["m_data"]
+    rows = d.numericTemplateArgument(value.type, 1)
+    cols = d.numericTemplateArgument(value.type, 2)
+    # Test for Eigen::Dynamic dimensions
+    if rows==-1:
+        rows = int(m_storage["m_rows"])
+    if cols==-1:
+        cols = int(m_storage["m_cols"])
+    size = rows*cols
+    if rows == 1 and cols == 1:
+        # Just emit single element
+        d.putItem( m_data[0][0] )
+    else:
+        # Emit rows x cols, with data array as child elements
+        d.putValue( "%dx%d" % (rows,cols) )
+        d.putNumChild(size)
+        if d.isExpanded():
+            innerType = d.templateArgument(value.type, 0)
+            # Check if m_data is an array or pointer on the heap
+            if m_data.TypeIsPointerType():
+                d.putPlotData(innerType, m_data, size)
+            else:
+                d.putPlotData(innerType, d.addressOf(value), size)
+
+# Eigen::Array, expected function name qdump__Eigen__Array
+# Eigen::Array is organised identically to Eigen::Matrix
+def qdump__Array(d, value):
+    qdump__Matrix(d, value)
---
+++ sophustypes.py
@@ -0,0 +1,41 @@
+############################################################################
+## Sophus (https://github.com/strasdat/Sophus) QtCreator Python Debug Helpers
+## Author: Steven Lovegrove
+############################################################################
+## These helpers have been tested with QT Creator 3.1.0 on OSX 10.9.2
+## Using lldb-310.2.37. My platform doesn't seem to recognise class namespaces,
+## and to function correctly these have been omitted.
+
+# Sophus::SO3Group, expected function name qdump__Sophus__SO3Group
+def qdump__SO3Group(d, value):
+    coeffs = value["unit_quaternion_"]["m_coeffs"]
+    d.putNumChild(1)
+    with Children(d):
+        d.putSubItem( "Unit Quaternion", coeffs )
+
+# Sophus::RxSO3Group, expected function name qdump__Sophus__RxSO3Group
+def qdump__RxSO3Group(d, value):
+    coeffs = value["quaternion_"]["m_coeffs"]
+    d.putNumChild(1)
+    with Children(d):
+        d.putSubItem( "Quaternion", coeffs )
+
+# Sophus::SE3Group, expected function name qdump__Sophus__SE3Group
+def qdump__SE3Group(d, value):
+    quat = value["so3_"]["unit_quaternion_"]["m_coeffs"]
+    trans = value["translation_"]
+    d.putValue("")
+    d.putNumChild(2)
+    with Children(d):
+        d.putSubItem( "Unit Quaternion", quat )
+        d.putSubItem( "Translation", trans )
+
+# Sophus::Sim3Group, expected function name qdump__Sophus__Sim3Group
+def qdump__Sim3Group(d, value):
+    quat = value["rxso3_"]["quaternion_"]["m_coeffs"]
+    trans = value["translation_"]
+    d.putValue("")
+    d.putNumChild(2)
+    with Children(d):
+        d.putSubItem( "Quaternion", quat )
+        d.putSubItem( "Translation", trans )
